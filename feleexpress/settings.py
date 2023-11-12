f3d05df75541645a8f0d1a8b3160bf0b3762c5c5
seyifunmi-idowu <seyiidowu24@yahoo.com>
2023-11-12 13:02:14 +0100

    s3 static

--- feleexpress/settings.py
+++ feleexpress/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "rest_framework",
     "drf_yasg",
+    "storages",
     "authentication.apps.AuthenticationConfig",
     "customer.apps.CustomerConfig",
     "rider.apps.RiderConfig",
@@ -126,7 +127,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+AWS_DEFAULT_REGION = config("AWS_DEFAULT_REGION", "")
+AWS_S3_BUCKET = config("AWS_S3_BUCKET", "")
+AWS_ACCESS_KEY = config("AWS_ACCESS_KEY", "")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", "")
+
+USE_S3 = config("USE_S3", cast=bool, default=True)
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY")
+    AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = config("AWS_S3_BUCKET")
+    AWS_DEFAULT_ACL = "public-read"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    # s3 static settings
+    AWS_LOCATION = "static"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+else:
+    STATIC_URL = "/static/"
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -176,7 +198,6 @@ SIMPLE_JWT = {
     "AUTH_TOKEN_CLASSES": ("helpers.token_manager.CustomAccessToken",),
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 ACCOUNT_SID = config("ACCOUNT_SID", "")
 AUTH_TOKEN = config("AUTH_TOKEN", "")
@@ -186,11 +207,6 @@ SMS_FROM = config("SMS_FROM", "")
 SENDGRID_API_KEY = config("SENDGRID_API_KEY", "")
 SENDER_EMAIL = config("SENDER_EMAIL", "")
 SENDER_NAME = config("SENDER_NAME", "")
-
-AWS_DEFAULT_REGION = config("AWS_DEFAULT_REGION", "")
-AWS_S3_BUCKET = config("AWS_S3_BUCKET", "")
-AWS_ACCESS_KEY = config("AWS_ACCESS_KEY", "")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", "")
 
 EMAIL_VERIFICATION_TTL = config("EMAIL_VERIFICATION_TTL", 21600, cast=int)
 EMAIL_VERIFICATION_MAX_TRIALS = config("EMAIL_VERIFICATION_MAX_TRIALS", 5, cast=int)
