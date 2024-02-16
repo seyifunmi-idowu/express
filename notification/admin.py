53d2da802c17a19030ab26e8fc452e96d4ae2114
seyifunmi-idowu <seyiidowu24@yahoo.com>
2024-02-16 11:03:00 +0100

    notifications

--- notification/admin.py
+++ notification/admin.py
@@ -1,10 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from notification.models import UserNotification
-
-# class UserNotificationAdmin(admin.ModelAdmin):
-# readonly_fields = ("user", "one_signal_id", "external_id")
-# exclude = ("state", "created_by", "deleted_by", "updated_by", "deleted_at")
+from notification.models import Notification
 
 
-# admin.site.register(UserNotification, UserNotificationAdmin)
+class NotificationAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at",)
+    exclude = ("state", "created_by", "deleted_by", "updated_by", "deleted_at")
+
+
+admin.site.register(Notification, NotificationAdmin)
